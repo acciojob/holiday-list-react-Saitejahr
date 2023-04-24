d9bf141e416f4c0ee42e5f61ecdbd1d6e20efe42
Saitejahr <98202897+Saitejahr@users.noreply.github.com>
2023-04-24 22:14:46 +0530

    submitting code-Mon Apr 24 2023 22:14:46 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
-    const cityList = [{ name: 'Goa', country: 'India' },
+    const cityList = [
+    { name: 'Goa', country: 'India' },
     { name: 'Amsterdam', country: 'Netherlands' },
     { name: 'New York', country: 'USA' },
     { name: 'Darjeeling', country: 'India' },
@@ -36,6 +37,36 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol>
+                <li>Goa</li>
+                <li>Amsterdam</li>
+                <li>New York</li>
+                <li>Darjeeling</li>
+                <li>Tokyo</li>
+                <li>Lonavala</li>
+                <li>Brandenburg Gate</li>
+                <li>Reichstag Building</li>
+                <li>Museum Island</li>
+                <li>Munnar</li>
+                <li>Leh Ladakh</li>
+                <li>Goa</li>
+                <li>Agra</li>
+                <li>Dalhousie</li>
+                <li>Coorg</li>
+                <li>Rome</li>
+                <li>Milan</li>
+                <li>Venice</li>
+                <li>Varanasai</li>
+                <li>Jaipur</li>
+                <li>The Hofburg</li>
+                <li>Belvedere Palace</li>
+                <li>St. Stephen Cathedral</li>
+                <li>Kahna National Park</li>
+                <li>Amritsar</li>
+                <li>Mussoorie</li>
+                <li>Mount Abu</li>
+                <li>Tirupati</li>
+               </ol>
     </div>
   )
 }
